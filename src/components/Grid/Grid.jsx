8389a6370566281e8d4097d6e95fa6bb0a86fe0e
JACKS9969 <101617802+JACKS9969@users.noreply.github.com>
2023-08-01 12:21:06 +0530

    new

--- src/components/Grid/Grid.jsx
+++ src/components/Grid/Grid.jsx
@@ -31,12 +31,13 @@ function Grid({numberOfCards}) {
                 winner && (
                     <>
                         <h1 className="turn-highlight">Winner is {winner}</h1>
-                        <button className="reset" onClick={reset}>Reset Game</button>
+                        
                     </>
                     
                 )
             }
-            <h1 className="turn-highlight">Current turn: {(true) ? 'O' : 'X'}</h1>
+            <button className="reset" onClick={reset}>Reset Game</button>
+            <h1 className="turn-highlight">Current turn: {(turn) ? 'O' : 'X'}</h1>
             <div className="grid">
                 {board.map((el, idx) => <Card  gameEnd={winner ? true : false} key={idx} onPlay={play} player={el} index={idx} />)}
             </div>
